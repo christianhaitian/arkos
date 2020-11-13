b1e829eb4f1dcf23aa6ae685562ef461966bb704
christianhaitian <mckman@comcast.net>
2020-11-13 00:51:20 -0500

    Update Update-RG351P.sh

--- Update-RG351P.sh
+++ Update-RG351P.sh
@@ -1,4 +1,45 @@
 #!/bin/bash
-msgbox "No updates available at this time.  Check back later."
-rm -- "$0"
-exit 187
+clear
+
+UPDATE_DATE="11132020"
+LOG_FILE="/home/ark/update$UPDATE_DATE.log"
+UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
+
+if [ -f "$UPDATE_DONE" ]; then
+	msgbox "No more updates available.  Check back later."
+	rm -- "$0"
+	exit 187
+fi
+
+if [ -f "$LOG_FILE" ]; then
+	sudo rm "$LOG_FILE"
+fi
+
+c_brightness="$(cat /sys/devices/platform/backlight/backlight/backlight/brightness)"
+sudo chmod 666 /dev/tty1
+echo 255 > /sys/devices/platform/backlight/backlight/backlight/brightness
+touch $LOG_FILE
+tail -f $LOG_FILE >> /dev/tty1 &
+
+if [ ! -f "$UPDATE_DONE" ]; then
+	printf "\nCorrect FAVORIS to FAVORITE at the bottom of the screen when inside a system menu...\n" | tee -a "$LOG_FILE"
+	sudo mv -v /usr/bin/emulationstation/emulationstation /usr/bin/emulationstation/emulationstation.update$UPDATE_DATE.bak | tee -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/11132020/emulationstation -O /usr/bin/emulationstation/emulationstation -a "$LOG_FILE"
+	sudo chmod -v 777 /usr/bin/emulationstation/emulationstation | tee -a "$LOG_FILE"
+	
+	printf "\nAdd missing genesis folder to EASYROMS directory if it doesn't already exist...\n" | tee -a "$LOG_FILE"
+	if [ ! -d "/roms/genesis/" ]; then
+		sudo mkdir -v /roms/genesis
+	fi
+
+	printf "\nFix input not shown when inserting username/password in scraper menu...\n" | tee -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/11132020/theme.xml -O /etc/emulationstation/themes/es-theme-nes-box/theme.xml -a "$LOG_FILE"
+	sudo chown -v ark:ark /etc/emulationstation/themes/es-theme-nes-box/theme.xml | tee -a "$LOG_FILE"
+	sudo chmod -v 664 /etc/emulationstation/themes/es-theme-nes-box/theme.xml | tee -a "$LOG_FILE"
+	msgbox "Updates have been completed.  You'll need to restart Emulationstation in order for some of these updates to take effect."
+	touch "$UPDATE_DONE"
+	rm -v -- "$0" | tee -a "$LOG_FILE"
+	printf "\033c" >> /dev/tty1
+	echo $c_brightness > /sys/devices/platform/backlight/backlight/backlight/brightness
+	exit 187
+fi
