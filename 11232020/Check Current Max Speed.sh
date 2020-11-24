f2c0bd29eb1254067f058bc8c18759ba891a7ca6
christianhaitian <mckman@comcast.net>
2020-11-24 02:04:15 -0500

    Update Check Current Max Speed.sh

--- 11232020/Check Current Max Speed.sh
+++ 11232020/Check Current Max Speed.sh
@@ -4,7 +4,7 @@ sudo rg351p-js2xbox --silent -t oga_joypad &
 sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 
-msgbox "$(lscpu | grep "CPU max MHz")"
+sudo msgbox "$(lscpu | grep "CPU max MHz")"
 
 sudo kill $(pidof rg351p-js2xbox)
 sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
