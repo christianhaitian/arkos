6482096a6fd4900cbbf119e1a9b3e565f3ec19c6
christianhaitian <mckman@comcast.net>
2020-11-24 21:47:15 -0500

    Update

---
+++ 11242020/Disable Wifi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo nmcli r wifi off
+printf "\n\n\n\e[32mWifi has been disabled.\n"
+sleep 1
---
+++ 11242020/Enable Wifi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo nmcli r wifi on
+printf "\n\n\n\e[32mWifi has been enabled.\n"
+sleep 1
+
