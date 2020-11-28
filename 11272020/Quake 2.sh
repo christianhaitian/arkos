d89aa1457e287076713a84815d70fb3866f58ce4
christianhaitian <mckman@comcast.net>
2020-11-27 23:30:38 -0500

    Update Quake 2.sh

--- 11272020/Quake 2.sh
+++ 11272020/Quake 2.sh
@@ -1,1 +1,1 @@
-/usr/local/bin/retroarch32 -L /home/odroid/.config/retroarch32/cores/vitaquake2_libretro.so /roms/ports/quake2/baseq2/
+/usr/local/bin/retroarch32 -L /home/ark/.config/retroarch32/cores/vitaquake2_libretro.so /roms/ports/quake2/baseq2/
