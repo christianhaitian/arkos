61eba51923ee0538a5bb872bbc20d90579388f66
christianhaitian <mckman@comcast.net>
2020-12-08 02:43:48 -0500

    Update

--- Update-RG351P.sh
+++ Update-RG351P.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-UPDATE_DATE="12032020"
+UPDATE_DATE="12072020"
 LOG_FILE="/home/ark/update$UPDATE_DATE.log"
 UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
 
@@ -449,7 +449,7 @@ if [ ! -f "/home/ark/.config/.update12012020" ]; then
 	touch "/home/ark/.config/.update12012020"
 fi
 
-if [ ! -f "$UPDATE_DONE" ]; then
+if [ ! -f "/home/ark/.config/.update12032020" ]; then
 
 	printf "\nRevert GBA(lr-mgba) core with rumble support to previous version...\n" | tee -a "$LOG_FILE"
 	sudo wget https://github.com/christianhaitian/arkos/raw/main/12032020/mgba_libretro.so -O /home/ark/.config/retroarch/cores/mgba_libretro.so -a "$LOG_FILE"
@@ -460,6 +460,64 @@ if [ ! -f "$UPDATE_DONE" ]; then
 	sudo mkdir -v /usr/bin/emulationstation/resources/locale/pt/ | tee -a "$LOG_FILE"
 	sudo wget https://github.com/christianhaitian/arkos/raw/main/12032020/pt/emulationstation2.po -O /usr/bin/emulationstation/resources/locale/pt/emulationstation2.po -a "$LOG_FILE"
 	
+	touch "/home/ark/.config/.update12032020"
+fi
+
+if [ ! -f "$UPDATE_DONE" ]; then
+
+	printf "\nFix some Emulationstation menu translations...\n" | tee -a "$LOG_FILE"
+	sudo rm -vf /usr/bin/emulationstation/resources/locale/pt/emulationstation2.po | tee -a "$LOG_FILE"
+	sudo rm -vf /usr/bin/emulationstation/resources/locale/br/emulationstation2.po | tee -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/EmulationStation-fcamod/raw/master/resources/locale/pt/emulationstation2.po -O /usr/bin/emulationstation/resources/locale/pt/emulationstation2.po -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/EmulationStation-fcamod/raw/master/resources/locale/br/emulationstation2.po -O /usr/bin/emulationstation/resources/locale/br/emulationstation2.po -a "$LOG_FILE"
+
+	printf "\nFix wrong resolution for Vitaquake2 to fix initial performance if need be...\n" | tee -a "$LOG_FILE"
+	cat /home/ark/.config/retroarch32/retroarch-core-options.cfg | grep vitaquakeii_resolution
+	if [ $? -ne 0 ]; then
+		sudo sed -i '/tyrquake_rumble/c\tyrquake_rumble \= \"disabled\"\nvitaquakeii_resolution \= \"480x272\"' /home/ark/.config/retroarch32/retroarch-core-options.cfg
+	fi
+
+	printf "\nFix supported extensions for msx games and add and default emulator to bluemsx instead...\n" | tee -a "$LOG_FILE"
+	sudo cp -v /etc/emulationstation/es_systems.cfg /etc/emulationstation/es_systems.cfg.update12072020.bak | tee -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12072020/es_systems.cfg -O /home/ark/es_systems.cfg -a "$LOG_FILE"
+	if [ -f "/home/ark/es_systems.cfg" ]; then
+		sudo mv -v /home/ark/es_systems.cfg /etc/emulationstation/es_systems.cfg | tee -a "$LOG_FILE"
+	fi
+	sudo chmod -v 775 /etc/emulationstation/es_systems.cfg | tee -a "$LOG_FILE"
+	sudo chown -v ark:ark /etc/emulationstation/es_systems.cfg | tee -a "$LOG_FILE"
+
+	printf "\nAdd support for the right analog stick to standalone ppsspp emulator and permission fix...\n" | tee -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12072020/PPSSPPSDL -O /opt/ppsspp/PPSSPPSDL -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12072020/gamecontrollerdb.txt -O /opt/ppsspp/assets/gamecontrollerdb.txt -a "$LOG_FILE"
+	sudo chmod -v 777 /opt/ppsspp/PPSSPPSDL | tee -a "$LOG_FILE"
+	sudo chown -v ark:ark /opt/ppsspp/PPSSPPSDL | tee -a "$LOG_FILE"
+	sudo chown -v ark:ark /opt/ppsspp/assets/gamecontrollerdb.txt | tee -a "$LOG_FILE"
+
+	printf "\nAdd filebrowser web server...\n" | tee -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12072020/filebrowser -O /usr/local/bin/filebrowser -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12072020/filebrowser.db -O /home/ark/.config/filebrowser.db -a "$LOG_FILE"
+	sudo mkdir -v /home/ark/.config/img | tee -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12072020/logo.svg -O /home/ark/.config/img/logo.svg -a "$LOG_FILE"
+	sudo chmod -v 777 /usr/local/bin/filebrowser | tee -a "$LOG_FILE"
+	sudo chown -v ark:ark /home/ark/.config/filebrowser.db | tee -a "$LOG_FILE"
+	sudo chown -v ark:ark /home/ark/.config/img/logo.svg | tee -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12072020/Disable%20Remote%20Services.sh -O /opt/system/"Disable Remote Services".sh -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12072020/Enable%20Remote%20Services.sh -O /opt/system/"Enable Remote Services".sh -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12072020/filebrowser -O /usr/local/bin/filebrowser -a "$LOG_FILE"
+	sudo chmod -v 777 /opt/system/"Disable Remote Services".sh | tee -a "$LOG_FILE"
+	sudo chmod -v 777 /opt/system/"Enable Remote Services".sh | tee -a "$LOG_FILE"
+	sudo chown -v ark:ark /opt/system/"Disable Remote Services".sh | tee -a "$LOG_FILE"
+	sudo chown -v ark:ark /opt/system/"Enable Remote Services".sh | tee -a "$LOG_FILE"
+	
+	printf "\nUpdate boot text to reflect current version of ArkOS\n" | tee -a "$LOG_FILE"
+	sudo sed -i "/title\=/c\title\=ArkOS 1.4 ($UPDATE_DATE)" /usr/share/plymouth/themes/text.plymouth
+
+	printf "\nDisable systemd-resolved...\n" | tee -a "$LOG_FILE"
+	sudo systemctl disable systemd-resolved
+	sudo systemctl stop systemd-resolved
+	sudo cp -v /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf.update12072020.bak | tee -a "$LOG_FILE"
+	sudo sed -i "/plugins\=/c\plugins\=ifupdown\,keyfile\ndns\=default" /etc/NetworkManager/NetworkManager.conf
+
 	touch "$UPDATE_DONE"
 	rm -v -- "$0" | tee -a "$LOG_FILE"
 	printf "\033c" >> /dev/tty1
