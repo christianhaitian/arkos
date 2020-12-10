5cb85eb55835b996c7b3edc72713b34b9f2565ca
christianhaitian <mckman@comcast.net>
2020-12-10 00:02:29 -0500

    Update Update-RG351P.sh

--- Update-RG351P.sh
+++ Update-RG351P.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-UPDATE_DATE="12072020"
+UPDATE_DATE="12092020"
 LOG_FILE="/home/ark/update$UPDATE_DATE.log"
 UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
 
@@ -463,7 +463,7 @@ if [ ! -f "/home/ark/.config/.update12032020" ]; then
 	touch "/home/ark/.config/.update12032020"
 fi
 
-if [ ! -f "$UPDATE_DONE" ]; then
+if [ ! -f "/home/ark/.config/.update12072020" ]; then
 
 	printf "\nFix some Emulationstation menu translations...\n" | tee -a "$LOG_FILE"
 	sudo rm -vf /usr/bin/emulationstation/resources/locale/pt/emulationstation2.po | tee -a "$LOG_FILE"
@@ -519,6 +519,40 @@ if [ ! -f "$UPDATE_DONE" ]; then
 	#sudo cp -v /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf.update12072020.bak | tee -a "$LOG_FILE"
 	#sudo sed -i "/plugins\=/c\plugins\=ifupdown\,keyfile\ndns\=default" /etc/NetworkManager/NetworkManager.conf
 
+	touch "/home/ark/.config/.update12072020"
+fi
+
+if [ ! -f "$UPDATE_DONE" ]; then
+
+	printf "\nAdded non rumble enabled GBA cores (MGBA, VBA-M, VBA-NExt)...\n" | tee -a "$LOG_FILE"
+	sudo cp -v /etc/emulationstation/es_systems.cfg /etc/emulationstation/es_systems.cfg.update12092020.bak | tee -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12092020/es_systems.cfg -O /home/ark/es_systems.cfg -a "$LOG_FILE"
+	if [ -f "/home/ark/es_systems.cfg" ]; then
+		sudo mv -v /home/ark/es_systems.cfg /etc/emulationstation/es_systems.cfg | tee -a "$LOG_FILE"
+	fi
+	sudo chmod -v 775 /etc/emulationstation/es_systems.cfg | tee -a "$LOG_FILE"
+	sudo chown -v ark:ark /etc/emulationstation/es_systems.cfg | tee -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12092020/mgba_rumble_libretro.so -O /home/ark/.config/retroarch/cores/mgba_rumble_libretro.so -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12092020/mgba_libretro.so -O /home/ark/.config/retroarch/cores/mgba_libretro.so -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12092020/vbam_libretro.so -O /home/ark/.config/retroarch/cores/vbam_libretro.so -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12092020/vba_next_libretro.so -O /home/ark/.config/retroarch/cores/vba_next_libretro.so -a "$LOG_FILE"
+	sudo chmod -v 775 /home/ark/.config/retroarch/cores/mgba_rumble_libretro.so | tee -a "$LOG_FILE"
+	sudo chown -v ark:ark /home/ark/.config/retroarch/cores/mgba_rumble_libretro.so | tee -a "$LOG_FILE"
+	sudo chmod -v 775 /home/ark/.config/retroarch/cores/mgba_libretro.so | tee -a "$LOG_FILE"
+	sudo chown -v ark:ark /home/ark/.config/retroarch/cores/mgba_libretro.so | tee -a "$LOG_FILE"	
+	sudo chmod -v 775 /home/ark/.config/retroarch/cores/vbam_libretro.so | tee -a "$LOG_FILE"
+	sudo chown -v ark:ark /home/ark/.config/retroarch/cores/vbam_libretro.so | tee -a "$LOG_FILE"
+	sudo chmod -v 775 /home/ark/.config/retroarch/cores/vba_next_libretro.so | tee -a "$LOG_FILE"
+	sudo chown -v ark:ark /home/ark/.config/retroarch/cores/vba_next_libretro.so | tee -a "$LOG_FILE"
+	sudo rm -v /home/ark/.config/retroarch/cores/mgba_libretro.so.lck | tee -a "$LOG_FILE"
+
+	printf "\nUpdate boot text to reflect current version of ArkOS\n" | tee -a "$LOG_FILE"
+	sudo sed -i "/title\=/c\title\=ArkOS 1.4 ($UPDATE_DATE)" /usr/share/plymouth/themes/text.plymouth
+
+	printf "\nEnsure systemd-resolved has been reenabled...\n" | tee -a "$LOG_FILE"
+	sudo systemctl enable systemd-resolved
+	sudo systemctl start systemd-resolved
+	
 	touch "$UPDATE_DONE"
 	rm -v -- "$0" | tee -a "$LOG_FILE"
 	printf "\033c" >> /dev/tty1
