2c905f2514312a8ea2fc8739ac245149cbd2451f
christianhaitian <mckman@comcast.net>
2020-12-21 05:41:18 -0500

    Update

--- Update-RG351P.sh
+++ Update-RG351P.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-UPDATE_DATE="12192020-1"
+UPDATE_DATE="12212020"
 LOG_FILE="/home/ark/update$UPDATE_DATE.log"
 UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
 
@@ -832,7 +832,7 @@ if [ ! -f "/home/ark/.config/.update12192020" ]; then
 	touch "/home/ark/.config/.update12192020"
 fi
 
-if [ ! -f "$UPDATE_DONE" ]; then
+if [ ! -f "/home/ark/.config/.update12192020-1" ]; then
 
 	printf "\nRevert PPSSPP back to previous version...\n" | tee -a "$LOG_FILE"
 	sudo wget https://github.com/christianhaitian/arkos/raw/main/12192020-1/arkosupdate12192020-1.zip -O /home/ark/arkosupdate12192020-1.zip -a "$LOG_FILE"
@@ -854,6 +854,34 @@ if [ ! -f "$UPDATE_DONE" ]; then
 		exit 1
 	fi
 	
+	touch "/home/ark/.config/.update12192020-1"
+fi
+
+if [ ! -f "$UPDATE_DONE" ]; then
+
+	printf "\nReadd PPSSPPSDL and keep original PPSSPPGO\nAdd glide64mk2 plugin for mupen64plus\nUpdate lzdoom.ini to support Heretic, Hexen, Strife, and Chex Quest...\n" | tee -a "$LOG_FILE"
+	sudo wget https://github.com/christianhaitian/arkos/raw/main/12212020/arkosupdate12212020.zip -O /home/ark/arkosupdate12212020.zip -a "$LOG_FILE"
+	if [ -f "/home/ark/arkosupdate12212020.zip" ]; then
+		cp -r -f -v /opt/ppsspp /opt/ppssppgo | tee -a "$LOG_FILE"
+		cp -f -v /roms/psp/ppsspp/PSP/SYSTEM/ppsspp.ini /roms/psp/ppsspp/PSP/SYSTEM/ppsspp.ini.go
+		touch /home/ark/.config/.update12212020
+		sudo unzip -X -o /home/ark/arkosupdate12212020.zip -d / | tee -a "$LOG_FILE"
+		sudo rm -v /home/ark/arkosupdate12212020.zip | tee -a "$LOG_FILE"
+	else 
+		printf "\nThe update couldn't complete because the package did not download correctly.\nPlease retry the update again." | tee -a "$LOG_FILE"
+		echo $c_brightness > /sys/devices/platform/backlight/backlight/backlight/brightness
+		exit 1
+	fi
+
+	if [ -f "/home/ark/.config/.update12212020" ]; then
+	printf "\nUpdate boot text to reflect current version of ArkOS\n" | tee -a "$LOG_FILE"
+		sudo sed -i "/title\=/c\title\=ArkOS 1.5 ($UPDATE_DATE)" /usr/share/plymouth/themes/text.plymouth
+	else
+		printf "\nThe update couldn't complete because the package did not download correctly.\nPlease retry the update again." | tee -a "$LOG_FILE"
+		echo $c_brightness > /sys/devices/platform/backlight/backlight/backlight/brightness
+		exit 1
+	fi
+	
 	touch "$UPDATE_DONE"
 	rm -v -- "$0" | tee -a "$LOG_FILE"
 	printf "\033c" >> /dev/tty1
@@ -861,5 +889,4 @@ if [ ! -f "$UPDATE_DONE" ]; then
 	echo $c_brightness > /sys/devices/platform/backlight/backlight/backlight/brightness
 	sudo reboot
 	exit 187
-	
 fi
