5eddfbcd7cab994d3bf7035b57c53bc1b05d6855
christianhaitian <mckman@comcast.net>
2021-07-13 21:49:20 -0400

    Create Update-CHI.sh

---
+++ Update-CHI.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+clear
+
+UPDATE_DATE="07022021"
+LOG_FILE="/home/ark/update$UPDATE_DATE.log"
+UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
+
+if [ -f "$UPDATE_DONE" ]; then
+	msgbox "No more updates available.  Check back later."
+	rm -- "$0"
+	exit 187
+fi
+
+if [ -f "$LOG_FILE" ]; then
+	sudo rm "$LOG_FILE"
+fi
+
+sudo msgbox "ONCE YOU PROCEED WITH THIS UPDATE SCRIPT, DO NOT STOP THIS SCRIPT UNTIL IT IS COMPLETED OR THIS DISTRIBUTION MAY BE LEFT IN A STATE OF UNUSABILITY.  Make sure you've created a backup of this sd card as a precaution in case something goes very wrong with this process.  You've been warned!  Type OK in the next screen to proceed."
+my_var=`osk "Enter OK here to proceed." | tail -n 1`
+
+echo "$my_var" | tee -a "$LOG_FILE"
+
+if [ "$my_var" != "OK" ] && [ "$my_var" != "ok" ]; then
+  sudo msgbox "You didn't type OK.  This script will exit now and no changes have been made from this process."
+  printf "You didn't type OK.  This script will exit now and no changes have been made from this process." | tee -a "$LOG_FILE"
+  exit 187
+fi
+
+c_brightness="$(cat /sys/devices/platform/backlight/backlight/backlight/brightness)"
+sudo chmod 666 /dev/tty1
+echo 255 > /sys/devices/platform/backlight/backlight/backlight/brightness
+touch $LOG_FILE
+tail -f $LOG_FILE >> /dev/tty1 &
+
+if [ ! -f "$UPDATE_DONE" ]; then
+
+
+	touch "$UPDATE_DONE"
+	rm -v -- "$0" | tee -a "$LOG_FILE"
+	printf "\033c" >> /dev/tty1
+	msgbox "Updates have been completed.  System will now restart after you hit the A button to continue.  If the system doesn't restart after pressing A, just restart the system manually."
+	echo $c_brightness > /sys/devices/platform/backlight/backlight/backlight/brightness
+	sudo reboot
+	exit 187	
+fi
