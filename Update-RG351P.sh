7238b336b15b6b088a6d1c169ce0999a54df5dc9
Wayne Lymbery <wayne.lymbery@envirosoft.co>
2021-08-09 23:12:04 +1000

    revert slight problem with file permisions

--- Update-RG351P.sh
+++ Update-RG351P.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-UPDATE_DATE="08092021-1"
+UPDATE_DATE="07152021-1"
 LOG_FILE="/home/ark/update$UPDATE_DATE.log"
 UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
 
@@ -29,7 +29,7 @@ if [ ! -f "/home/ark/.config/.update07152021" ]; then
 	sudo wget --no-check-certificate https://github.com/wummle/arkos/raw/main/07152021/rg351p/arkosupdate07152021.zip -O /home/ark/arkosupdate07152021.zip -a "$LOG_FILE" || rm -f /home/ark/arkosupdate07152021.zip | tee -a "$LOG_FILE"
 	if [ -f "/home/ark/arkosupdate07152021.zip" ]; then
 		sudo unzip -X -o /home/ark/arkosupdate07152021.zip -d / | tee -a "$LOG_FILE"
-		sudo rm -v /home/ark/arkosupdate07152021.zip | tee -a "$LOG_FILE"
+		sudo rm -v /home/ark/arkosupdate05012021.zip | tee -a "$LOG_FILE"
 	else 
 		printf "\nThe update couldn't complete because the package did not download correctly.\nPlease retry the update again." | tee -a "$LOG_FILE"
 		sleep 3
@@ -46,32 +46,6 @@ if [ ! -f "/home/ark/.config/.update07152021" ]; then
 
 	touch "/home/ark/.config/.update07152021"
 fi
-
-
-if [ ! -f "/home/ark/.config/.update08092021" ]; then
-
-	printf "\nStreet of Rage Remake\n" | tee -a "$LOG_FILE"
-	sudo wget --no-check-certificate https://github.com/wummle/arkos/raw/main/08092021/arkosupdate08092021.zip -O /home/ark/arkosupdate08092021.zip -a "$LOG_FILE" || rm -f /home/ark/arkosupdate08092021.zip | tee -a "$LOG_FILE"
-	if [ -f "/home/ark/arkosupdate08092021.zip" ]; then
-		sudo unzip -X -o /home/ark/arkosupdate08092021.zip -d / | tee -a "$LOG_FILE"
-		sudo rm -v /home/ark/arkosupdate08092021.zip | tee -a "$LOG_FILE"
-	else 
-		printf "\nThe update couldn't complete because the package did not download correctly.\nPlease retry the update again." | tee -a "$LOG_FILE"
-		sleep 3
-		echo $c_brightness > /sys/devices/platform/backlight/backlight/backlight/brightness
-		exit 1
-	fi
-
-	printf "\nEnsure 64bit and 32bit sdl2 is still properly linked\n" | tee -a "$LOG_FILE"
-	sudo ln -sfv /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0.14.1 /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0 | tee -a "$LOG_FILE"
-	sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0.10.0 /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0 | tee -a "$LOG_FILE"
-
-	printf "\nUpdate boot text to reflect final current version of ArkOS for the 351 P/M \n" | tee -a "$LOG_FILE"
-	sudo sed -i "/title\=/c\title\=ArkOS 351P/M wummle" /usr/share/plymouth/themes/text.plymouth
-
-	touch "/home/ark/.config/.update07152021"
-fi
-
 
 if [ ! -f "$UPDATE_DONE" ]; then
 
