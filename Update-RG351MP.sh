81f99cc969a3aeaca7188c625103a9c547f3f3bd
christianhaitian <mckman@comcast.net>
2021-10-07 19:53:21 -0400

    Update

--- Update-RG351MP.sh
+++ Update-RG351MP.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-UPDATE_DATE="10022021"
+UPDATE_DATE="10072021"
 LOG_FILE="/home/ark/update$UPDATE_DATE.log"
 UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
 
@@ -118,7 +118,7 @@ if [ ! -f "/home/ark/.config/.update10012021-1" ]; then
 	touch "/home/ark/.config/.update10012021-1"
 fi
 
-if [ ! -f "$UPDATE_DONE" ]; then
+if [ ! -f "/home/ark/.config/.update10022021" ]; then
 
 	printf "\nUpdate u-boot for boot logo\nUpdate boot.ini\nUpdate kernel for experimental battery reading\n" | tee -a "$LOG_FILE"
 	sudo wget -t 3 -T 60 --no-check-certificate "$LOCATION"/10022021/arkosupdate10022021.zip -O /home/ark/arkosupdate10022021.zip -a "$LOG_FILE" || rm -f /home/ark/arkosupdate10022021.zip | tee -a "$LOG_FILE"
@@ -144,6 +144,51 @@ if [ ! -f "$UPDATE_DONE" ]; then
 	
 	printf "\nUpdate boot text to reflect current version of ArkOS\n" | tee -a "$LOG_FILE"
 	sudo sed -i "/title\=/c\title\=ArkOS 2.0 Test Release 1.4" /usr/share/plymouth/themes/text.plymouth
+
+	touch "/home/ark/.config/.update10022021"
+fi
+
+if [ ! -f "$UPDATE_DONE" ]; then
+
+	printf "\nUpdate u-boot to remove rumble on boot\nSwap .orig and .tony dtbs\nUpdate retroarch and retroarch32 to 1.9.10\nAdd support for Satellaview\n" | tee -a "$LOG_FILE"
+	sudo wget -t 3 -T 60 --no-check-certificate "$LOCATION"/10072021/arkosupdate10072021.zip -O /home/ark/arkosupdate10072021.zip -a "$LOG_FILE" || rm -f /home/ark/arkosupdate10072021.zip | tee -a "$LOG_FILE"
+	if [ -f "/home/ark/arkosupdate10072021.zip" ]; then
+		cp -v /opt/retroarch/bin/retroarch /opt/retroarch/bin/retroarch.198.bak | tee -a "$LOG_FILE"
+		cp -v /opt/retroarch/bin/retroarch32 /opt/retroarch/bin/retroarch32.198.bak | tee -a "$LOG_FILE"
+		sudo unzip -X -o /home/ark/arkosupdate10072021.zip -d / | tee -a "$LOG_FILE"
+		mkdir -v /roms/satellaview | tee -a "$LOG_FILE"
+		cp -v /etc/emulationstation/es_systems.cfg /etc/emulationstation/es_systems.cfg.update10072021.bak | tee -a "$LOG_FILE"
+		if test -z "$(cat /etc/emulationstation/es_systems.cfg | grep satellaview)"
+		then
+		  sed -i -e '/<theme>sufami<\/theme>/{r /home/ark/add_satellaview.txt' -e 'd}' /etc/emulationstation/es_systems.cfg
+		fi
+		if [ -f "/opt/system/Advanced/Switch to main SD for Roms.sh" ]; then
+		  mkdir -v /roms2/satellaview | tee -a "$LOG_FILE"
+		  sed -i '/<path>\/roms\//s//<path>\/roms2\//g' /etc/emulationstation/es_systems.cfg
+		fi
+		cd /home/ark/sd_fuse
+		sudo dd if=idbloader.img of=/dev/mmcblk0 conv=notrunc bs=512 seek=64 | tee -a "$LOG_FILE"
+		sudo dd if=uboot.img of=/dev/mmcblk0 conv=notrunc bs=512 seek=16384 | tee -a "$LOG_FILE"
+		sudo dd if=trust.img of=/dev/mmcblk0 conv=notrunc bs=512 seek=24576 | tee -a "$LOG_FILE"
+		sudo mv -f -v rg351mp-kernel.dtb /boot/rg351mp-kernel.dtb | tee -a "$LOG_FILE"
+		sync
+		cd /home/ark
+		sudo rm -v /home/ark/add_satellaview.txt | tee -a "$LOG_FILE"
+		sudo rm -v /home/ark/arkosupdate10072021.zip | tee -a "$LOG_FILE"
+		sudo rm -rf -v /home/ark/sd_fuse | tee -a "$LOG_FILE"
+	else 
+		printf "\nThe update couldn't complete because the package did not download correctly.\nPlease retry the update again." | tee -a "$LOG_FILE"
+		sleep 3
+		echo $c_brightness > /sys/devices/platform/backlight/backlight/backlight/brightness
+		exit 1
+	fi
+
+	printf "\nMake sure the proper SDLs are still linked\n" | tee -a "$LOG_FILE"
+	sudo ln -sfv /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0.10.0 /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0 | tee -a "$LOG_FILE"
+	sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0.10.0 /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0 | tee -a "$LOG_FILE"
+	
+	printf "\nUpdate boot text to reflect current version of ArkOS\n" | tee -a "$LOG_FILE"
+	sudo sed -i "/title\=/c\title\=ArkOS 2.0 Test Release 1.5" /usr/share/plymouth/themes/text.plymouth
 
 	touch "$UPDATE_DONE"
 	rm -v -- "$0" | tee -a "$LOG_FILE"
