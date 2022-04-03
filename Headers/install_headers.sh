368bd93ea76a922c86ced9afd3e63046c3169161
christianhaitian <mckman@comcast.net>
2022-04-03 02:39:34 -0400

    Update install_headers.sh

--- Headers/install_headers.sh
+++ Headers/install_headers.sh
@@ -9,6 +9,10 @@ fi
 
 sudo dpkg -i linux-headers-4.4.189_4.4.189-2_arm64.deb
 
+# Apply some patches to fix some possible compile issues with gcc 9
+wget -t 3 -T 60 --no-check-certificate https://github.com/christianhaitian/arkos/raw/main/Headers/module.patch -O - | sudo patch
+wget -t 3 -T 60 --no-check-certificate https://github.com/christianhaitian/arkos/raw/main/Headers/compiler.patch -O - | sudo patch
+
 # Fix vermagic description so it properly matches
 sudo sed -i "/#define UTS_RELEASE/c\#define UTS_RELEASE \"4.4.189\"" /usr/src/linux-headers-4.4.189/include/generated/utsrelease.h
 
