7c5fd1fc9969c5dcbb438b8e9f133f6643469015
christianhaitian <mckman@comcast.net>
2022-04-03 00:12:37 -0400

    Update install_headers.sh

--- Headers/install_headers.sh
+++ Headers/install_headers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -t 3 -T 60 --no-check-certificate https://github.com/christianhaitian/arkos/raw/main/KernelandHeader/linux-headers-4.4.189_4.4.189-2_arm64.deb || rm -f linux-headers-4.4.189_4.4.189-2_arm64.deb
+wget -t 3 -T 60 --no-check-certificate https://github.com/christianhaitian/arkos/raw/main/Headers/linux-headers-4.4.189_4.4.189-2_arm64.deb || rm -f linux-headers-4.4.189_4.4.189-2_arm64.deb
 
 if [ ! -f "linux-headers-4.4.189_4.4.189-2_arm64.deb" ]; then
 	printf "\nThe linux header deb did not download correctly or is missing. Either rerun this script or manually download it from the git and place it in this current folder then run this script again.\n"
@@ -10,6 +10,6 @@ fi
 sudo dpkg -i linux-headers-4.4.189_4.4.189-2_arm64.deb
 sudo apt update -y && sudo apt-get --reinstall install -y build-essential bc bison flex libssl-dev python linux-libc-dev libc6-dev
 cd /usr/src/linux-headers-4.4.189/
-wget -t 3 -T 60 --no-check-certificate https://github.com/christianhaitian/arkos/raw/main/KernelandHeader/headers-debian-byteshift.patch -O - | sudo patch -p1
+wget -t 3 -T 60 --no-check-certificate https://github.com/christianhaitian/arkos/raw/main/Headers/headers-debian-byteshift.patch -O - | sudo patch -p1
 sudo make scripts
 cd ~
