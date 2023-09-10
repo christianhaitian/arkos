c4362af8eff608f54513731804cf67042ca1ba55
christianhaitian <mckman@comcast.net>
2023-09-10 00:39:18 -0400

    Create Update-RGB30.sh

---
+++ Update-RGB30.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+clear
+UPDATE_DATE="08112023"
+LOG_FILE="/home/ark/update$UPDATE_DATE.log"
+UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
+
+if [ -f "$UPDATE_DONE" ]; then
+	msgbox "No more updates available.  Check back later."
+	rm -- "$0"
+	exit 187
+fi
+
+if [ -f "$LOG_FILE" ]; then
+	sudo rm "$LOG_FILE"
+fi
+
+LOCATION="http://gitcdn.link/cdn/christianhaitian/arkos/main"
+ISITCHINA="$(curl -s --connect-timeout 30 -m 60 http://demo.ip-api.com/json | grep -Po '"country":.*?[^\\]"')"
+
+if [ "$ISITCHINA" = "\"country\":\"China\"" ]; then
+  printf "\n\nSwitching to China server for updates.\n\n" | tee -a "$LOG_FILE"
+  LOCATION="http://139.196.213.206/arkos"
+fi
+
+sudo msgbox "ONCE YOU PROCEED WITH THIS UPDATE SCRIPT, DO NOT STOP THIS SCRIPT UNTIL IT IS COMPLETED OR THIS DISTRIBUTION MAY BE LEFT IN A STATE OF UNUSABILITY.  Make sure you've created a backup of this sd card as a precaution in case something goes very wrong with this process.  You've been warned!  Type OK in the next screen to proceed."
+my_var=`osk "Enter OK here to proceed." | tail -n 1`
+
+echo "$my_var" | tee -a "$LOG_FILE"
+
+if [ "$my_var" != "OK" ] && [ "$my_var" != "ok" ]; then
+  sudo msgbox "You didn't type OK.  This script will exit now and no changes have been made from this process."
+  printf "You didn't type OK.  This script will exit now and no changes have been made from this process." | tee -a "$LOG_FILE"
+  exit 187
+fi
+
+c_brightness="$(cat /sys/class/backlight/backlight/brightness)"
+sudo chmod 666 /dev/tty1
+echo 255 > /sys/class/backlight/backlight/brightness
+touch $LOG_FILE
+tail -f $LOG_FILE >> /dev/tty1 &
+
+
